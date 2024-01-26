fcee3c7a56d1938fd8c550b0ab5fbf61d2481877
mcnwr76 <anwaraan998@gmail.com>
2024-01-26 22:06:42 +0700

    update

--- src/pages/HubunganInvestor/index.jsx
+++ src/pages/HubunganInvestor/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import { Button, Img, Line, List, Text } from "components";
 import Footer from "components/Footer";
@@ -23,25 +23,31 @@ const HubunganInvestorPage = () => {
     "Presentasi Kepada Investor",
     "Laporan Keberlanjutan",
   ];
-  const ButtonTab = ({ title, index }) => {
-    return (
-      <Button
-        className="cursor-pointer font-medium h-10 min-w-[306px] rounded-[20px] text-base text-center tracking-[0.40px]"
-        color="gray_700"
-        size="xs"
-        variant="outline"
-        onClick={() => setTab(index)}
-      >
-        {title}
-      </Button>
-    );
-  };
+
+  const ButtonTab = useCallback(
+    ({ title, index }) => {
+      return (
+        <Button
+          className={`${
+            index === tab ? "bg-[#FFA132]" : ""
+          } cursor-pointer font-medium h-10 min-w-[306px] rounded-[20px] text-base text-center tracking-[0.40px]`}
+          color="gray_700"
+          size="xs"
+          variant="outline"
+          onClick={() => setTab(index)}
+        >
+          {title}
+        </Button>
+      );
+    },
+    [tab]
+  );
 
   return (
     <>
-      <div className="bg-gray-100 font-nunito sm:gap-10 md:gap-10 gap-[200px] items-center w-full">
-        <Header className="flex relative mx-auto w-full items-center px-4 md:px-24 py-12" />
-        <div className="flex flex-col p-4 md:p-24">
+      <div className="bg-gray-100 font-nunito">
+        <Header className="flex sticky top-0 bg-gray-100 z-10 mx-auto w-full items-center px-[12%] pt-8" />
+        <div className="flex flex-col px-[12%] pt-16 md:pt-20">
           <div className="flex flex-col gap-4 items-start">
             <Text
               className="sm:text-[40px] md:text-[46px] text-[50px] text-center text-gray-800 tracking-[0.15px]"
@@ -60,7 +66,7 @@ const HubunganInvestorPage = () => {
           <div className="flex flex-col gap-[52px] items-start mt-[89px] w-full">
             <div className="flex overflow-scroll w-full gap-2 items-start">
               {page.map((title, i) => (
-                <ButtonTab title={title} index={i + 1} />
+                <ButtonTab key={title} title={title} index={i + 1} />
               ))}
             </div>
             {tab === 1 ? (
